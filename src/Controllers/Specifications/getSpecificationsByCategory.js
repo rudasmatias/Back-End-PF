d5ee81bdd1860421f0f328424218ea629129dc6b
NicolasOviedoRubiano <nicolas.or97@gmail.com>
2023-08-31 17:12:03 -0500

    relaction category-sepcs, and specByCategory

--- src/Controllers/Specifications/getSpecificationsByCategory.js
+++ src/Controllers/Specifications/getSpecificationsByCategory.js
@@ -1,1 +1,20 @@
-const { specificaionModel } = require("../../db");
+const { specificationModel, Specification, Categories } = require("../../db");
+
+const getSpecsByCategoryController = async (id) => {
+  const specs = await Specification.findAll({
+    include: [
+      { model: Categories, where: { id_categoria: id }, attributes: [] },
+    ],
+  });
+  return specs;
+};
+
+const getSpecByCategory = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const specs = await getSpecsByCategoryController(id);
+    const response = { message: "todo ok", specifications: specs };
+    res.status(200).json(response);
+  } catch (error) {}
+};
+module.exports = getSpecByCategory;
--- src/Routes/Specifications/SpecificationRouter.js
+++ src/Routes/Specifications/SpecificationRouter.js
@@ -1,7 +1,8 @@
 const { Router } = require("express");
+const getSpecByCategory = require("../../Controllers/Specifications/getSpecificationsByCategory");
 
 const specificationRouter = Router();
 
-specificationRouter.get("/");
+specificationRouter.get("/:id", getSpecByCategory);
 
 module.exports = specificationRouter;
