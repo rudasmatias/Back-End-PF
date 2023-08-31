8bd2d2ecc74a6adc3cc134d4a45965c13c7c5834
NicolasOviedoRubiano <nicolas.or97@gmail.com>
2023-08-31 15:23:18 -0500

    postProduct corregido

--- src/Controllers/Products/postProducts.js
+++ src/Controllers/Products/postProducts.js
@@ -1,25 +1,46 @@
-const { Products } = require("../../db");
+const { Products, Images } = require("../../db");
 
 const postProducts = async (req, res) => {
   try {
-    const { nombre, calificacion, precio, descuento, stock, id_categoria, imagen } = req.body;
+    const {
+      nombre,
+      calificacion,
+      precio,
+      descuento,
+      stock,
+      id_categoria,
+      imagen,
+    } = req.body;
 
-    if(!nombre || !calificacion || !precio || !descuento || !stock || !id_categoria || !imagen) return res.status(400).send("Falta informacion");
+    if (
+      !nombre ||
+      !calificacion ||
+      !precio ||
+      !descuento ||
+      !stock ||
+      !id_categoria ||
+      !imagen
+    )
+      return res.status(400).send("Falta informacion");
     const newProduct = await Products.create({
-        nombre,
-        calificacion,
-        precio,
-        descuento,
-        stock,
-        garantia: 12,
-        iva: 10.5,
-        id_categoria
+      nombre,
+      calificacion,
+      precio,
+      descuento,
+      stock,
+      garantia: 12,
+      iva: 10.5,
+      id_categoria,
     });
-    
-    await newProduct.addImages({url: imagen});
 
-    return res.status(201).json({message: 'Producto creado exitosamente'})
+    await Images.findOrCreate({
+      where: {
+        url: imagen,
+        id_product: newProduct.id_producto,
+      },
+    });
 
+    return res.status(201).json({ message: "Producto creado exitosamente" });
   } catch (error) {
     return res.status(500).json({ message: error });
   }
--- src/db.js
+++ src/db.js
@@ -20,18 +20,18 @@ const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 ? path __filename: la variable basename contendrá el nombre del archivo sin la ruta del directorio, es decir, solo el nombre del archivo en sí. Esto puede ser útil en escenarios donde necesitas referenciar el nombre del archivo actual sin la ruta completa*/
 
 //*mySQL
-// const sequelize = new Sequelize(
-//   `mysql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
-//   { logging: false, native: false, host: "localhost", dialect: "mysql" }
-// );
-//*Postgress
 const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
-  {
-    logging: false,
-    native: false,
-  }
+  `mysql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+  { logging: false, native: false, host: "localhost", dialect: "mysql" }
 );
+//*Postgress
+// const sequelize = new Sequelize(
+//   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+//   {
+//     logging: false,
+//     native: false,
+//   }
+// );
 
 const basename = path.basename(__filename);
 
