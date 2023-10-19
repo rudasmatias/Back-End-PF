e12e2ab1478887101c8c66f47f99269c71861aae
Matias Rudas <ronaldmatiasrudas@gmail.com>
2023-10-19 20:07:10 -0300

    server a false

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const DB_connect = require("./src/DB_connect/DB_connect");
 const PORT = process.env.PORT || 3001;
 
 conn
-  .sync({ force: true })
+  .sync({ force: false })
   .then(async () => {
     console.log("♥ Conectado a la base de datos ♥");
     await DB_connect();
